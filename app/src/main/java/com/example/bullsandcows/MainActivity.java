9441507061abe1c6abd27f922a9c062f394a9d9f
1820709 <56402921+1820709@users.noreply.github.com>
2019-11-04 00:47:03 +0000

    Added functionality to calculate and display bulls and cows

--- app/src/main/java/com/example/bullsandcows/MainActivity.java
+++ app/src/main/java/com/example/bullsandcows/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     Random rnd;
     int guessA = 1, guessB = 2, guessC = 3, guessD = 4;
     int randomA, randomB, randomC, randomD;
-    int bulls = 0, cows = 0;
+    int bulls = 0, cows = 0, tries = 0;
+    String outputTxt = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         btn_minus_b = (Button) findViewById(R.id.btn_minus_b);
         btn_minus_c = (Button) findViewById(R.id.btn_minus_c);
         btn_minus_d = (Button) findViewById(R.id.btn_minus_d);
+
         btn_plus_a = (Button) findViewById(R.id.btn_plus_a);
         btn_plus_b = (Button) findViewById(R.id.btn_plus_b);
         btn_plus_c = (Button) findViewById(R.id.btn_plus_c);
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         txt_number_b = (TextView) findViewById(R.id.txt_number_b);
         txt_number_c = (TextView) findViewById(R.id.txt_number_c);
         txt_number_d = (TextView) findViewById(R.id.txt_number_d);
+
         txt_info = (TextView) findViewById(R.id.txt_info);
         txt_output = (TextView) findViewById(R.id.txt_output);
     }
@@ -57,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
         do {
             randomB = rnd.nextInt(9) + 1;
-        } while (randomA == randomB);
+        } while (randomB == randomA);
 
         do {
             randomC = rnd.nextInt(9) + 1;
@@ -171,9 +174,15 @@ public class MainActivity extends AppCompatActivity {
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                btn_reset.setEnabled(false);
-                btn_match.setEnabled(false);
+                guessA = 1;
+                guessB = 2;
+                guessC = 3;
+                guessD = 4;
                 txt_info.setText("Try again! The number was: " + randomA+randomB+randomC+randomD);
+                txt_number_a.setText(""+guessA);
+                txt_number_b.setText(""+guessB);
+                txt_number_c.setText(""+guessC);
+                txt_number_d.setText(""+guessD);
             }
         });
 
@@ -185,13 +194,53 @@ public class MainActivity extends AppCompatActivity {
                         || guessB==guessD || guessC==guessD){
                     txt_info.setText("All numbers must be different!");
                 } else {
-                    matchNumber();
+                    tries++;
+                    matchBulls();
+                    matchCows();
+                    outputTxt = outputTxt + tries + ". " + guessA + guessB + guessC + guessD +
+                            " - Bulls: " + bulls + ", Cows: " + cows + "\n";
+                    txt_output.setText(outputTxt);
+                    bulls = 0;
+                    cows = 0;
                 }
             }
         });
     }
 
-    private void matchNumber() {
-        
+    private void matchBulls() {
+        //bulls
+        if(guessA == randomA) {
+            bulls++;
+        }
+        if(guessB == randomB) {
+            bulls++;
+        }
+        if(guessC == randomC) {
+            bulls++;
+        }
+        if(guessD == randomD) {
+            bulls++;
+        }
+        if(bulls == 4){
+            //btn_reset.setEnabled(false);
+            //btn_match.setEnabled(false);
+            txt_info.setText("Congratulations!");
+        }
+    }
+
+    private void matchCows() {
+        //bulls
+        if(guessA == randomB || guessA == randomC || guessA == randomD) {
+            cows++;
+        }
+        if(guessB == randomA || guessB == randomC || guessB == randomD) {
+            cows++;
+        }
+        if(guessC == randomA || guessC == randomB || guessC == randomD) {
+            cows++;
+        }
+        if(guessD == randomA || guessD == randomB || guessD == randomC) {
+            cows++;
+        }
     }
 }
